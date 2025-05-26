072a22065e3febec2853dd691cc5c1614987b551
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-05-26 12:32:48 +0700

    membuat main kemudian membuat object serta memberi value kepada masing masing object dan mendisplay semua object

--- ConstructorOverloading.cpp
+++ ConstructorOverloading.cpp
@@ -33,3 +33,15 @@ void mahasiswa::cetak(){
     cout << endl << "NIM = " << Nim << endl;
     cout << "Nama = " << Nama << endl;
 }
+
+int main() {
+    mahasiswa mhs1(102030);             // Hanya NIM
+    mahasiswa mhs2("Andi");             // Hanya Nama
+    mahasiswa mhs3(10203, "Santi");     // NIM dan Nama
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
